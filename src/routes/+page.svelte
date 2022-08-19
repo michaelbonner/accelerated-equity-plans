e6c93e6dcbf8ac8153d0d972e5bc57857a72d74a
Michael Bonner <michaelwbonner@gmail.com>
2022-08-19 14:25:39 -0600

    real basic landing page with tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-[90vh] w-full justify-center items-center">
+	<div class="px-8 py-12 max-w-lg text-center grid gap-8">
+		<img
+			src="/images/accelerated-equity-plans.png"
+			alt="Accelerated Equity Plans logo"
+			width="484"
+			height="381"
+		/>
+	</div>
+</div>
+<h1 class="max-w-5xl mx-auto font-light">
+	&copy;{new Date().getFullYear()} Accelerated Equity Plans, LLC
+</h1>
