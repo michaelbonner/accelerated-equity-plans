5a35edd803c40faa72e9540718868af6721036c9
Michael Bonner <michaelwbonner@gmail.com>
2022-08-19 14:57:44 -0600

    lighten up the black colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,54 +2,56 @@
 	<title>Accelerated Equity Plans</title>
 </svelte:head>
 
-<div class="flex min-h-[90vh] w-full justify-center items-center">
-	<div class="px-8 py-8 md:py-12 max-w-lg text-center grid gap-16">
-		<div class="grid gap-4 md:gap-8 text-center">
-			<img
-				class="mx-auto scale-90 md:scale-100"
-				src="/images/accelerated-equity-plans-mark.png"
-				alt="Accelerated Equity Plans logo"
-				width="191"
-				height="191"
-			/>
-			<h1 class="font-serif uppercase">
-				<span class="block text-4xl md:text-5xl font-bold">Accelerated</span>
-				<span class="block text-3xl md:text-4xl font-light">Equity Plans</span>
-			</h1>
-		</div>
-		<div class="grid gap-12 md:gap-24">
-			<h1 class="sr-only">Accelerated Equity Plans</h1>
-			<p class="md:text-lg font-light">
-				Accelerated Equity Plans helps you create, manage, and implement equity compensation plans.
-				We’ll help you create a plan that’s right for your business and your needs.
-			</p>
-			<div class="grid gap-4">
-				<h2 class="text-3xl font-bold font-serif">Get in touch</h2>
-				<p>
-					<a
-						class="flex flex-wrap gap-2 underline underline-offset-2 items-center justify-center"
-						href="https://linkedin.com/in/emily-bone-mba-cep"
-					>
-						<svg
-							stroke="currentColor"
-							fill="currentColor"
-							stroke-width="0"
-							viewBox="0 0 24 24"
-							height="1em"
-							width="1em"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								fill-rule="evenodd"
-								d="M20.45175,20.45025 L16.89225,20.45025 L16.89225,14.88075 C16.89225,13.5525 16.86975,11.844 15.04275,11.844 C13.191,11.844 12.90825,13.2915 12.90825,14.7855 L12.90825,20.45025 L9.3525,20.45025 L9.3525,8.997 L12.765,8.997 L12.765,10.563 L12.81375,10.563 C13.2885,9.66225 14.4495,8.71275 16.18125,8.71275 C19.78575,8.71275 20.45175,11.08425 20.45175,14.169 L20.45175,20.45025 Z M5.33925,7.4325 C4.1955,7.4325 3.27375,6.50775 3.27375,5.36775 C3.27375,4.2285 4.1955,3.30375 5.33925,3.30375 C6.47775,3.30375 7.4025,4.2285 7.4025,5.36775 C7.4025,6.50775 6.47775,7.4325 5.33925,7.4325 L5.33925,7.4325 Z M7.11975,20.45025 L3.5565,20.45025 L3.5565,8.997 L7.11975,8.997 L7.11975,20.45025 Z M23.00025,0 L1.0005,0 C0.44775,0 0,0.44775 0,0.99975 L0,22.9995 C0,23.55225 0.44775,24 1.0005,24 L23.00025,24 C23.55225,24 24,23.55225 24,22.9995 L24,0.99975 C24,0.44775 23.55225,0 23.00025,0 L23.00025,0 Z"
-							/></svg
-						>
-						<span> linkedin.com/in/emily-bone-mba-cep </span>
-					</a>
+<div class="text-gray-800">
+	<div class="flex min-h-[90vh] w-full justify-center items-center">
+		<div class="px-8 py-8 md:py-12 max-w-lg text-center grid gap-16">
+			<div class="grid gap-4 md:gap-8 text-center">
+				<img
+					class="mx-auto scale-90 md:scale-100 opacity-70"
+					src="/images/accelerated-equity-plans-mark.png"
+					alt="Accelerated Equity Plans logo"
+					width="191"
+					height="191"
+				/>
+				<h1 class="font-serif uppercase">
+					<span class="block text-4xl md:text-5xl font-bold">Accelerated</span>
+					<span class="block text-3xl md:text-4xl font-light">Equity Plans</span>
+				</h1>
+			</div>
+			<div class="grid gap-12 md:gap-24">
+				<h1 class="sr-only">Accelerated Equity Plans</h1>
+				<p class="md:text-lg font-light">
+					Accelerated Equity Plans helps you create, manage, and implement equity compensation
+					plans. We’ll help you create a plan that’s right for your business and your needs.
 				</p>
+				<div class="grid gap-4">
+					<h2 class="text-3xl font-bold font-serif">Get in touch</h2>
+					<p>
+						<a
+							class="flex flex-wrap gap-2 underline underline-offset-2 items-center justify-center"
+							href="https://linkedin.com/in/emily-bone-mba-cep"
+						>
+							<svg
+								stroke="currentColor"
+								fill="currentColor"
+								stroke-width="0"
+								viewBox="0 0 24 24"
+								height="1em"
+								width="1em"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									fill-rule="evenodd"
+									d="M20.45175,20.45025 L16.89225,20.45025 L16.89225,14.88075 C16.89225,13.5525 16.86975,11.844 15.04275,11.844 C13.191,11.844 12.90825,13.2915 12.90825,14.7855 L12.90825,20.45025 L9.3525,20.45025 L9.3525,8.997 L12.765,8.997 L12.765,10.563 L12.81375,10.563 C13.2885,9.66225 14.4495,8.71275 16.18125,8.71275 C19.78575,8.71275 20.45175,11.08425 20.45175,14.169 L20.45175,20.45025 Z M5.33925,7.4325 C4.1955,7.4325 3.27375,6.50775 3.27375,5.36775 C3.27375,4.2285 4.1955,3.30375 5.33925,3.30375 C6.47775,3.30375 7.4025,4.2285 7.4025,5.36775 C7.4025,6.50775 6.47775,7.4325 5.33925,7.4325 L5.33925,7.4325 Z M7.11975,20.45025 L3.5565,20.45025 L3.5565,8.997 L7.11975,8.997 L7.11975,20.45025 Z M23.00025,0 L1.0005,0 C0.44775,0 0,0.44775 0,0.99975 L0,22.9995 C0,23.55225 0.44775,24 1.0005,24 L23.00025,24 C23.55225,24 24,23.55225 24,22.9995 L24,0.99975 C24,0.44775 23.55225,0 23.00025,0 L23.00025,0 Z"
+								/></svg
+							>
+							<span> linkedin.com/in/emily-bone-mba-cep </span>
+						</a>
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
+	<p class="max-w-5xl mx-auto font-light text-center pb-4">
+		&copy;{new Date().getFullYear()} Accelerated Equity Plans, LLC
+	</p>
 </div>
-<p class="max-w-5xl mx-auto font-light text-center pb-4">
-	&copy;{new Date().getFullYear()} Accelerated Equity Plans, LLC
-</p>
