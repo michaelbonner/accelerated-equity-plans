d8c85ef0c0f93177527afde820e3e241aad3ec70
Michael Bonner <michaelwbonner@gmail.com>
2022-08-19 19:17:14 -0600

    search console meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,7 @@
 		href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Lora:wght@400;500;700&display=swap"
 		rel="preload"
 	/>
+	<meta name="google-site-verification" content="P9yZQUWa7MG_9CkMXW0nuZ0yUkn95LNLzUQMdNJOBtU" />
 </svelte:head>
 
 <slot />
