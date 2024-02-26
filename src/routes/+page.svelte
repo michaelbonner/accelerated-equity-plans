aa36b9a894207441a27edca24cf499d8bf0d17e1
androosk <atirpok3@gmail.com>
2024-02-26 16:12:23 -0700

    added cta button down the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -217,6 +217,27 @@
 
 	<!-- End Services Section -->
 
+	<!-- Start CTA Section -->
+	<section>
+    <div class="relative isolate overflow-hidden bg-gray-900 px-6 py-14 text-center shadow-2xl sm:px-16 flex items-center justify-around">
+      <h2 class="text-3xl font-bold tracking-wide text-white sm:text-4xl">Take your equity management to the next level today</h2>
+      <div class="flex items-center justify-center gap-x-6">
+        <a href="/" class="inline-block rounded-lg w-40 text-center bg-red-700 bg-opacity-30 px-4 py-1.5 text-base font-semibold leading-7 text-white ring-1 ring-red-700 hover:bg-red-800 hover:ring-red-800 no-underline">Learn More</a>
+      </div>
+      <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
+        <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7" />
+        <defs>
+          <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+            <stop stop-color="#7775D6" />
+            <stop offset="1" stop-color="#E935C1" />
+          </radialGradient>
+        </defs>
+      </svg>
+    </div>
+	</section>
+
+	<!-- End CTA Section -->
+
 	<!-- Start Testimonials Section -->
 
 	<section class="relative isolate overflow-hidden bg-white px-6 py-14 sm:py-32 lg:px-8">
