c0c1d05eb9accfd557fce0920d23a86e1f37ea57
Michael Bonner <michaelwbonner@gmail.com>
2024-06-07 11:40:37 -0600

    Updated contact form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,21 @@ module.exports = {
 				sans: ['roboto', ...defaultTheme.fontFamily.sans],
 				serif: ['Lora', ...defaultTheme.fontFamily.serif],
 				eurostile: ['eurostile', 'sans-serif']
+			},
+			colors: {
+				'aep-red': {
+					50: '#fdf3f3',
+					100: '#fde3e4',
+					200: '#fcccce',
+					300: '#f8a9ac',
+					400: '#f2777c',
+					500: '#e74c53',
+					600: '#d42e35',
+					700: '#ac2228',
+					800: '#932126',
+					900: '#7b2125',
+					950: '#420d0f'
+				}
 			}
 		}
 	},
