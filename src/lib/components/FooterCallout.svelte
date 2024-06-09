72d7d8148383c30e4aad398b900b42350e4b3869
Michael Bonner <michaelwbonner@gmail.com>
2024-06-09 12:26:42 -0600

    fix pattern not matching up

--- src/lib/components/FooterCallout.svelte
+++ src/lib/components/FooterCallout.svelte
@@ -3,17 +3,18 @@
 </script>
 
 <section
-	class="py-24 md:py-32 flex flex-col justify-center gap-8 text-center min-h-[50vh] px-6"
+	class="relative py-24 md:py-32 flex flex-col justify-center gap-8 text-center min-h-[50vh] px-6"
 	style={`
 				background-image: url('/images/patterns/Pattern-0224_Pattern-A-Repeated.svg');
-				background-size: 85%;
+				background-size: 80%;
 			`}
 >
-	<div class="max-w-2xl mx-auto">
+	<div class="bg-white/30 absolute inset-0" />
+	<div class="relative max-w-2xl mx-auto">
 		<h2 class={styles.h2}>Take your equity management <wbr />to the next level today</h2>
 	</div>
 
-	<div class="flex justify-center">
+	<div class="relative flex justify-center">
 		<a href="/contact" class={styles.redButton}>Contact us</a>
 	</div>
 </section>
