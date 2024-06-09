791016b916deab427123d6083f600d8b7446964a
Michael Bonner <michaelwbonner@gmail.com>
2024-06-09 12:27:49 -0600

    remove shadow

--- src/lib/components/CustomerReviews.svelte
+++ src/lib/components/CustomerReviews.svelte
@@ -7,7 +7,7 @@
 
 <section class="bg-black py-24 md:py-32">
 	<div
-		class="relative isolate overflow-hidden px-6 text-center shadow-2xl sm:px-16 flex flex-col items-center justify-between text-white"
+		class="relative isolate overflow-hidden px-6 text-center sm:px-16 flex flex-col items-center justify-between text-white"
 	>
 		<div class="grid gap-2">
 			<RedBar classes="mx-auto" />
