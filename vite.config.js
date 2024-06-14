d7a158599749fb6787e83b2f0558ab058c08b21f
Michael Bonner <michaelwbonner@gmail.com>
2024-06-14 00:39:07 -0600

    sort imports

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { partytownVite } from '@builder.io/partytown/utils';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { join } from 'path';
 
 /** @type {import('vite').UserConfig} */
