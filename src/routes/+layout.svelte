9fe800fa8fb4b021c03ea20592b3a2d1b7daeb55
Michael Bonner <michaelwbonner@gmail.com>
2024-06-14 00:29:32 -0600

    use partytown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 
-	import Analytics from '$lib/analytics.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import { partytownSnippet } from '@builder.io/partytown/integration';
 	import { onMount } from 'svelte';
 	import { AiOutlineContacts } from 'svelte-icons-pack/ai';
 	import { BiCustomize, BiHomeAlt } from 'svelte-icons-pack/bi';
@@ -61,9 +61,44 @@
 		content="AEP brings industry expertise and support to deliver a best-in-class equity program experience for teams, partners, and participants."
 	/>
 	<meta name="twitter:image" content="https://www.acceleratedep.com/images/og-image.jpg" />
+
+	<script>
+		// Forward the necessary functions to the web worker layer
+		partytown = {
+			forward: ['dataLayer.push', 'gtag'],
+			resolveUrl: (url) => {
+				const siteUrl = 'https://www.acceleratedep.com/proxytown';
+
+				if (url.hostname === 'www.googletagmanager.com') {
+					const proxyUrl = new URL(`${siteUrl}/gtm`);
+
+					const gtmId = new URL(url).searchParams.get('id');
+					gtmId && proxyUrl.searchParams.append('id', gtmId);
+
+					return proxyUrl;
+				}
+
+				return url;
+			}
+		};
+	</script>
+
+	{@html '<script>' + partytownSnippet() + '</script>'}
+
+	<script
+		type="text/partytown"
+		src="https://www.googletagmanager.com/gtag/js?id=G-252472179V"
+	></script>
+	<script type="text/partytown">
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function () {
+			dataLayer.push(arguments);
+		};
+		gtag('js', new Date());
+		gtag('config', 'G-252472179V');
+	</script>
 </svelte:head>
 
-<Analytics />
 <div class="flex flex-col min-h-screen">
 	<Navbar {navLinks} />
 	<div class="flex flex-col flex-grow bg-white">
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import { partytownVite } from '@builder.io/partytown/utils';
+import { join } from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [enhancedImages(), sveltekit()]
+	plugins: [
+		enhancedImages(),
+		sveltekit(),
+		partytownVite({
+			dest: join(__dirname, 'static', '~partytown')
+		})
+	]
 };
 
 export default config;
