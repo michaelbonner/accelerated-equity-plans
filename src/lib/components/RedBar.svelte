0ea211efd4659798f07d1c443524f533d6aad6a9
Michael Bonner <michaelwbonner@gmail.com>
2024-06-19 11:14:40 -0600

    add aria-hidden="true" to red bar

--- src/lib/components/RedBar.svelte
+++ src/lib/components/RedBar.svelte
@@ -2,7 +2,7 @@
 	export let classes = '';
 </script>
 
-<div class={classes}>
+<div aria-hidden="true" class={classes}>
 	<svg width="104" height="7" viewBox="0 0 104 7" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path d="M0.25 0.714355H100.25L103.75 6.71436H0.25V0.714355Z" fill="#AC2228" />
 	</svg>
