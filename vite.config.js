bdd3988de37744d8e605d3b3326ef7d1ba77212b
Michael Bonner <mike@bootpackdigital.com>
2025-07-13 11:20:57 -0600

    Update tailwind setup

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { partytownVite } from '@qwik.dev/partytown/utils';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { join } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		tailwindcss(),
 		enhancedImages(),
 		sveltekit(),
 		partytownVite({
